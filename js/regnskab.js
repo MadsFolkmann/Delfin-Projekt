dabaca5028bde0a4ebecca30c59c3d7b4fc6e955
MadsFolkmann <mads1238@gmail.com>
2023-05-22 18:55:03 +0200

    if statements til regnskab

--- js/regnskab.js
+++ js/regnskab.js
@@ -32,16 +32,51 @@ function displayMembers(listOfMembers) {
 
 function showMembers(member) {
   console.log("hej");
-  const html = /*html*/ `
-    <tr>
-      <td>${member.name}</td>
-      <td>${member.membership}</td>
-      <td>1000</td>
-      <td>1000</td>
-      <td>0</td>
-    </tr>
-  `;
+  if (member.membership === "18+") {
+    const html = /*html*/ `
+      <tr>
+        <td>${member.name}</td>
+        <td>${member.membership}</td>
+        <td>1600</td>
+        <td>1600</td>
+        <td class="debt">0</td>
+      </tr>
+    `;
+    document.querySelector("#members").insertAdjacentHTML("beforeend", html);
+  } else if (member.membership === "u18") {
+        const html = /*html*/ `
+      <tr>
+        <td>${member.name}</td>
+        <td>${member.membership}</td>
+        <td>1000</td>
+        <td>1000</td>
+        <td class="debt">0</td>
+      </tr>
+    `;
+        document.querySelector("#members").insertAdjacentHTML("beforeend", html);
+  } else if (member.membership === "pensionist") {
+        const html = /*html*/ `
+      <tr>
+        <td>${member.name}</td>
+        <td>${member.membership}</td>
+        <td>1200</td>
+        <td>1200</td>
+        <td class="debt">0</td>
+      </tr>
+    `;
+        document.querySelector("#members").insertAdjacentHTML("beforeend", html);
+  } else {
+        const html = /*html*/ `
+      <tr>
+        <td>${member.name}</td>
+        <td>${member.membership}</td>
+        <td>500</td>
+        <td>500</td>
+        <td class="debt">0</td>
+      </tr>
+    `;
+        document.querySelector("#members").insertAdjacentHTML("beforeend", html);
+  }
 
-  document.querySelector("#members").insertAdjacentHTML("beforeend", html);
 //   document.querySelector("#members tr:last-child").addEventListener("click", () => membersClicked(member));
 }
